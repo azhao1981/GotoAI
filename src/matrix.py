fd75c029645ea952427bdd647b606f10d15791eb
azhao1981 <azhao.1981@gmail.com>
2017-11-17 01:34:57 +0800

    quick fix

--- src/matrix.py
+++ src/matrix.py
@@ -1,19 +1,22 @@
 """ 矩阵例子 """
 import numpy as np
 
-matrix_a = np.mat('4 3; 2 1')
-matrix_b = np.mat('1 2; 3 4')
+matrix_a = np.mat('4 3 2; 2 1 4; 1 2 3')
+matrix_b = np.mat('1 2 3; 2 3 4; 3 4 1')
 
 print(matrix_a)
-# [[4 3]
-#  [2 1]]
+[[4 3 2]
+ [2 1 4]
+ [1 2 3]]
 print(matrix_b)
-# [[1 2]
-#  [3 4]]
-print(matrix_a*matrix_b)
-# [[13 20]
-#  [ 5  8]]
+[[1 2 3]
+ [2 3 4]
+ [3 4 1]]
+print(matrix_a * matrix_b)
+[[16 25 26]
+ [16 23 14]
+ [14 20 14]]
 
 print(matrix_a - matrix_b)
 
-print(matrix_a * matrix_b)
+print(matrix_a + matrix_b)
